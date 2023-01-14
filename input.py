105c7c84ef3a96824668489157e7808e2e1e492a
AstragoDE <51675359+AstragoDE@users.noreply.github.com>
2023-01-15 00:33:14 +0100

    -

---
+++ input.py
@@ -0,0 +1,11 @@
+# prompts a question and waits for user input
+name = input("What is your name? ")
+
+print("Hello, " + name + "! We are happy to have you here :)")
+
+
+# Task:
+# ======
+#
+# Write a program that asks the user for his/her name and age.
+# Format the inputs into a sentence and print them to the console simmilar to the example above.
