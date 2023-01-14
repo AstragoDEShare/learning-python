a87c2880f80c8f4b24e44ab38caee202a31b368f
AstragoDE <51675359+AstragoDE@users.noreply.github.com>
2023-01-15 00:27:07 +0100

    -

--- variables.py
+++ variables.py
@@ -1,9 +1,19 @@
 # A program that greets people
 
-firstName = "Gustavo"  # "Gustavo" is saved to the variable called firstName
+firstName = "Elio"  # "Elio" is saved to the variable called firstName
 
 print("Hello, " + firstName)  # Greeting a person
 
 firstName = "Oliver"  # A new name is assigned to firstName
 
 print("Hello, " + firstName)  # Greeting a person (again)
+
+
+# Using Variables with Numbers
+
+num1 = 5.3
+num2 = 7
+
+num1 = 3
+
+print(num1 + num2)
---
+++ variables2.py
@@ -0,0 +1,12 @@
+# Reassigning variables programatically
+
+number = 5
+print(number)
+
+# Add 3 to the number
+number += 3
+
+# Also possible:
+# number = number + 3
+
+print(number)
